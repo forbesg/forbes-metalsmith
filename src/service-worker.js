5c1d976eedc596a3f167c3844fd42502767df679
Forbes Gray <fnbg75@gmail.com>
2019-04-23 15:19:30 +0100

    Remove commented code

--- src/service-worker.js
+++ src/service-worker.js
@@ -2,45 +2,16 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.2.0/workbox
 
 if (workbox) {
   workbox.precaching.precacheAndRoute([])
-  // workbox.routing.registerRoute(
-  //   // Cache CSS files.
-  //   /\.css$/,
-  //   // Use cache but update in the background.
-  //   new workbox.strategies.StaleWhileRevalidate({
-  //     // Use a custom cache name.
-  //     cacheName: 'css-cache',
-  //   })
-  // )
-  // workbox.routing.registerRoute(
-  //   // Cache CSS files.
-  //   /\.js$/,
-  //   // Use cache but update in the background.
-  //   new workbox.strategies.StaleWhileRevalidate({
-  //     // Use a custom cache name.
-  //     cacheName: 'js-cache',
-  //   })
-  // )
-  // workbox.routing.registerRoute(
-  //   // Cache CSS files.
-  //   /\.html$/,
-  //   // Use cache but update in the background.
-  //   new workbox.strategies.StaleWhileRevalidate({
-  //     // Use a custom cache name.
-  //     cacheName: 'html-cache',
-  //   })
-  // )
+
   workbox.routing.registerRoute(
-    // Cache image files.
+    // Cache font files.
     /\.(?:eot|svg|ttf|woff|woff2)$/,
     // Use the cache if it's available.
     new workbox.strategies.CacheFirst({
-      // Use a custom cache name.
       cacheName: 'font-cache',
       plugins: [
         new workbox.expiration.Plugin({
-          // Cache only 20 images.
           maxEntries: 15,
-          // Cache for a maximum of a week.
           maxAgeSeconds: 7 * 24 * 60 * 60,
         })
       ],
@@ -51,13 +22,10 @@ if (workbox) {
     /\.(?:png|jpg|jpeg|svg|gif)$/,
     // Use the cache if it's available.
     new workbox.strategies.CacheFirst({
-      // Use a custom cache name.
       cacheName: 'image-cache',
       plugins: [
         new workbox.expiration.Plugin({
-          // Cache only 20 images.
           maxEntries: 20,
-          // Cache for a maximum of a week.
           maxAgeSeconds: 7 * 24 * 60 * 60,
         })
       ],
